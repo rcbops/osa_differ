b4f377d4cfc69d327e43e4156b4f016a278a2d08
Major Hayden <major@mhtx.net>
2016-08-17 14:06:31 -0500

    Remove pytest-git

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,2 @@
 """Conftest for pytest."""
 import pytest
-
-
-# @pytest.fixture(autouse=True)
-# def no_requests(monkeypatch):
-#     """Disable requests module network capability."""
-#     monkeypatch.delattr("requests.sessions.Session.request")
--- tests/test_osa_differ.py
+++ tests/test_osa_differ.py
@@ -1,5 +1,6 @@
 """Testing osa-differ."""
 import argparse
+from git import Repo
 import httpretty
 import json
 from osa_differ import osa_differ
@@ -15,7 +16,7 @@ class TestOSADiffer(object):
             parser = osa_differ.create_parser()
             parser.parse_args([])
         out, err = capsys.readouterr()
-        assert "too few arguments" in err
+        assert "usage" in err
 
     def test_arguments_sufficient(self):
         """Verify that we get args properly with right args passed."""
@@ -55,79 +56,89 @@ class TestOSADiffer(object):
         result = osa_differ.get_commit_url(repo_url)
         assert result == repo_url
 
-    def test_get_commits(self, git_repo):
+    def test_get_commits(self, tmpdir):
         """Verify that we can get commits for a repo."""
-        path = git_repo.workspace
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
         for x in range(0, 10):
-            file = path / "test{1}.txt".format(path, x)
-            file.write_text("Test")
-            git_repo.run('git add test{0}.txt'.format(x))
-            git_repo.api.index.commit("Commit #{0}".format(x))
+            file = p / "test{0}.txt".format(x)
+            file.write_text("Test", encoding='ascii')
+            repo.index.add(['test{0}.txt'.format(x)])
+            repo.index.commit("Commit #{0}".format(x))
 
-        commits = osa_differ.get_commits(git_repo.workspace, 'HEAD~2', 'HEAD')
+        commits = osa_differ.get_commits(path, 'HEAD~2', 'HEAD')
         assert len(list(commits)) == 2
 
-    def test_get_commits_hide_merges(self, git_repo):
+    def test_get_commits_hide_merges(self, tmpdir):
         """Verify that we can get commits for a repo."""
-        path = git_repo.workspace
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
         for x in range(0, 10):
-            file = path / "test{1}.txt".format(path, x)
-            file.write_text("Test")
-            git_repo.run('git add test{0}.txt'.format(x))
-            git_repo.api.index.commit("Merge #{0}".format(x))
+            file = p / "test{0}.txt".format(x)
+            file.write_text("Test", encoding='ascii')
+            repo.index.add(['test{0}.txt'.format(x)])
+            repo.index.commit("Merge #{0}".format(x))
 
-        commits = osa_differ.get_commits(git_repo.workspace, 'HEAD~2', 'HEAD',
+        commits = osa_differ.get_commits(path, 'HEAD~2', 'HEAD',
                                          hide_merges=True)
         assert len(list(commits)) == 0
 
-    def test_get_commits_include_merges(self, git_repo):
+    def test_get_commits_include_merges(self, tmpdir):
         """Verify that we can get commits for a repo."""
-        path = git_repo.workspace
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
         for x in range(0, 10):
-            file = path / "test{1}.txt".format(path, x)
-            file.write_text("Test")
-            git_repo.run('git add test{0}.txt'.format(x))
-            git_repo.api.index.commit("Merge #{0}".format(x))
+            file = p / "test{0}.txt".format(x)
+            file.write_text("Test", encoding='ascii')
+            repo.index.add(['test{0}.txt'.format(x)])
+            repo.index.commit("Merge #{0}".format(x))
 
-        commits = osa_differ.get_commits(git_repo.workspace, 'HEAD~2', 'HEAD',
+        commits = osa_differ.get_commits(path, 'HEAD~2', 'HEAD',
                                          hide_merges=False)
         assert len(list(commits)) == 2
 
-    def test_get_projects(self, git_repo):
+    def test_get_projects(self, tmpdir):
         """Verify that we can retrieve projects."""
-        path = git_repo.workspace
-        file = path / 'test1.yml'
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test1.yml'
         file.write_text("""---
 tempest_git_repo: https://git.openstack.org/openstack/tempest
 tempest_git_install_branch: 1493c7f0ba49bfccb9ff8516b10a65d949d7462e
 tempest_git_project_group: utility_all
-""")
-        file = path / 'test2.yml'
+""", encoding='ascii')
+        file = p / 'test2.yml'
         file.write_text("""---
 novncproxy_git_repo: https://github.com/kanaka/novnc
 novncproxy_git_install_branch: da82b3426c27bf1a79f671c5825d68ab8c0c5d9f
 novncproxy_git_project_group: nova_console
-""")
-        git_repo.run('git add test1.yml')
-        git_repo.api.index.commit("Test")
-        projects = osa_differ.get_projects(git_repo.workspace,
+""", encoding='ascii')
+        repo.index.add(['test1.yml', 'test2.yml'])
+        repo.index.commit("Test")
+        projects = osa_differ.get_projects(path,
                                            ['test1.yml', 'test2.yml'],
                                            'HEAD')
         assert isinstance(projects, dict)
 
-    def test_get_roles(self, git_repo):
+    def test_get_roles(self, tmpdir):
         """Verify that we can get OSA role information."""
-        path = git_repo.workspace
-        file = path / 'ansible-role-requirements.yml'
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'ansible-role-requirements.yml'
         file.write_text("""
 - name: apt_package_pinning
   scm: git
   version: master
-""")
-        git_repo.run('git add ansible-role-requirements.yml')
-        git_repo.api.index.commit("Test")
+""", encoding='ascii')
+        repo.index.add(['ansible-role-requirements.yml'])
+        repo.index.commit("Test")
 
-        roles = osa_differ.get_roles(git_repo.workspace, 'HEAD')
+        roles = osa_differ.get_roles(path, 'HEAD')
         assert isinstance(roles, list)
         assert roles[0]['name'] == 'apt_package_pinning'
 
@@ -153,103 +164,118 @@ novncproxy_git_project_group: nova_console
         result = osa_differ.post_gist("report text", 'HEAD~1', 'HEAD')
         assert result == 'https://example.com/'
 
-    def test_commit_valid(self, git_repo):
+    def test_commit_valid(self, tmpdir):
         """Verify that we can find valid commits."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
-        result = osa_differ.valid_commit(git_repo.workspace, 'HEAD')
+        result = osa_differ.valid_commit(path, 'HEAD')
         assert result
 
-    def test_commit_invalid(self, git_repo):
+    def test_commit_invalid(self, tmpdir):
         """Verify that we can find valid commits."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
-        result = osa_differ.valid_commit(git_repo.workspace, 'HEAD~1')
+        result = osa_differ.valid_commit(path, 'HEAD~1')
         assert not result
 
-    def test_repo_clone(self, git_repo, monkeypatch):
+    def test_repo_clone(self, tmpdir, monkeypatch):
         """Verify that we can clone a repo."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
         def mockclone(x, y):
-            return git_repo
+            return path
 
         def mockwait(*args, **kwargs):
             return True
 
         monkeypatch.setattr("git.repo.base.Repo.clone", mockclone)
         monkeypatch.setattr("git.cmd.Git.AutoInterrupt.wait", mockwait)
-        result = osa_differ.repo_clone(git_repo.workspace,
+        result = osa_differ.repo_clone(path,
                                        "http://example.com")
 
         assert result.active_branch.name == 'master'
         assert not result.is_dirty()
 
-    def test_repo_clone_update(self, git_repo, tmpdir):
+    def test_repo_clone_update(self, tmpdir):
         """Verify that we can clone a repo."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
-        p = tmpdir.mkdir("test")
+        p = tmpdir.mkdir("test2")
         repo_path = p / "test"
-        result = osa_differ.update_repo(str(repo_path), git_repo.workspace)
+        path_clonefrom = str(repo_path)
+        result = osa_differ.update_repo(path, path_clonefrom)
 
         assert result.active_branch.name == 'master'
         assert not result.is_dirty()
 
-    def test_repo_update_update(self, git_repo, tmpdir):
+    def test_repo_update_update(self, tmpdir):
         """Verify that update_repo tries to update the repo."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
-        result = osa_differ.update_repo(git_repo.workspace, git_repo.workspace)
+        result = osa_differ.update_repo(path, path)
 
         assert result.active_branch.name == 'master'
         assert not result.is_dirty()
 
-    def test_repo_update_without_fetch(self, git_repo):
+    def test_repo_update_without_fetch(self, tmpdir):
         """Verify that we can get a repo ready without fetching it."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
 
-        result = osa_differ.repo_pull(git_repo.workspace,
+        result = osa_differ.repo_pull(path,
                                       "http://example.com",
                                       fetch=False)
         assert result.active_branch.name == 'master'
         assert not result.is_dirty()
 
-    def test_repo_update_with_fetch(self, git_repo, monkeypatch):
+    def test_repo_update_with_fetch(self, tmpdir, monkeypatch):
         """Verify that we can get a repo ready and update it."""
-        path = git_repo.workspace
-        file = path / 'test.txt'
-        file.write_text('Testing')
-        git_repo.run('git add test.txt')
-        git_repo.api.index.commit("Testing")
-        git_repo.api.create_remote('origin', git_repo.workspace)
+        p = tmpdir.mkdir('test')
+        path = str(p)
+        repo = Repo.init(path)
+        file = p / 'test.txt'
+        file.write_text('Testing', encoding='ascii')
+        repo.index.add(['test.txt'])
+        repo.index.commit('Testing')
+        repo.create_remote('origin', url='http://example.com')
 
         monkeypatch.setattr("git.remote.Remote.pull", lambda x: True)
-        result = osa_differ.repo_pull(git_repo.workspace,
+        result = osa_differ.repo_pull(path,
                                       "http://example.com",
                                       fetch=True)
         assert result.active_branch.name == 'master'
